d990b200bf71944da4b3d8bb838c6325b2c758d9
Unknown <DTruong054@west-mec.org>
2018-09-19 15:26:21 -0700

    _1oos pa
    Discofrd

---
+++ musicScale.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<!-- 
+    Exercise-02_01_01
+    Author: Daniel Truong
+    Date: 9.10.18
+    File: DiceRoll.php -->
+
+<head>
+    <meta charset="utf-8" />
+    <title>music scale</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="modernizr.custom.65897.js"></script>
+</head>
+
+<body>
+    <h2>Music Scale</h2>
+    <?php
+        $musicScale = array("do", "re", "me", "fa", "so", "la", "ti", "do");
+        $outputString = "The notes for music scale are: ";
+    ?>
+</body>
+
+</html>
